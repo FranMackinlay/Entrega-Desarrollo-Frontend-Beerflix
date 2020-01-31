621aae9f54e475ac5966d53aaa5a082420faccb3
Francisco Mackinlay <franmackinlay@gmail.com>
2020-01-31 16:34:05 -0300

    development mode webpack

--- webpack-basic/index.js
+++
@@ -1,1 +0,0 @@
-console.log('Webpack 2da edicion');
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path'); // es una libreria de node nativa.
 // require es una palabra reservada de commonJs y vale para importar librerias
 
 module.exports = {
-  entry: path.join(__dirname, 'webpack-basic', 'index.js'),
+  mode: 'development',
+  entry: path.join(__dirname, 'webpack-basic', 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'webpack-basic', 'build'),
     filename: 'bundle.js'
+    // filename: 'bundle.[hash].js'
   }
 };
