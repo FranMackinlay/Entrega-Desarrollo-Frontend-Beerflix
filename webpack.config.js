04f46a762692e59ff8673749599bb25778b6e38d
Francisco Mackinlay <franmackinlay@gmail.com>
2020-02-01 00:01:23 -0300

    HtmlWebpackPlugin and webpack-dev-server installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path'); // es una libreria de node nativa.
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // require es una palabra reservada de commonJs y vale para importar librerias
 
 module.exports = {
@@ -6,8 +9,8 @@ module.exports = {
   entry: path.join(__dirname, 'webpack-basic', 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'webpack-basic', 'build'),
-    filename: 'bundle.js'
-    // filename: 'bundle.[hash].js'
+    // filename: 'bundle.js'
+    filename: 'bundle.[hash].js'
   },
   module: {
     rules: [
@@ -33,5 +36,17 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'webpack-basic', 'index.html')
+    })
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'webpack-basic')
   }
 };
